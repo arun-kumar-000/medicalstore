a253be5a14dcf40910ff1a18446fb32004051f38
arun.kumar <your-email@example.com>
2025-07-28 11:43:30 +0530

    second commit

--- medicalstore/src/main/java/com/rotary/medicalstore/MedicalstoreApplication.java
+++ medicalstore/src/main/java/com/rotary/medicalstore/MedicalstoreApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedicalstoreApplication {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(MedicalstoreApplication.class, args);
 	}
-
+ 
 }
